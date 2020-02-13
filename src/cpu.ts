b2f437005f02531993498ee5ddb23389f219ce8c
ozaki <oja.bitterlife.club@gmail.com>
2020-02-13 16:47:55 +0000

    memoryの型を変更

--- src/cpu.ts
+++ src/cpu.ts
@@ -3,7 +3,7 @@ import { Op } from './op';
 
 export namespace Cpu {
     export function step(gb: GB.env) {
-        const op_code = gb.mem.rom[gb.regs.pc];
+        const op_code = gb.mem[gb.regs.pc];
         gb.regs.pc += 1;
 
         console.log((gb.regs.pc-1).toString(16) + ": " + op_code.toString(16));
--- src/memory.ts
+++ src/memory.ts
@@ -1,23 +1,21 @@
 export namespace Memory {
-    export type Memory = {
-        rom: Uint8Array;
-    }
+    export type Memory = Uint8Array;
 
     export function create(rom_: Uint8Array): Memory {
-        return {
-            rom: rom_,
-        };
+        let mem = new Uint8Array(65536);
+        for(let i = 0; i < rom_.length && i < mem.length; i++) mem[i] = rom_[i];
+        return mem;
     }
 
     export function readUByte(mem: Memory, addr: number): number {
-        return mem.rom[addr];
+        return mem[addr];
     }
     export function readSByte(mem: Memory, addr: number): number {
-        if(mem.rom[addr]&0x80) return -(((mem.rom[addr]^0xff)+1)&0xff);
-        return mem.rom[addr];
+        if(mem[addr]&0x80) return -(((mem[addr]^0xff)+1)&0xff);
+        return mem[addr];
     }
     export function readWord(mem: Memory, addr: number) : number{
-        return (mem.rom[addr+1] << 8) | mem.rom[addr];
+        return (mem[addr+1] << 8) | mem[addr];
     }
 
 }
