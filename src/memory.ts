2b7283c9d86c6807b24cf8da9c0c32aba3634cd3
ozaki <oja.bitterlife.club@gmail.com>
2020-02-20 16:30:18 +0000

    未実装のIO等が叩かれたら例外を出すように

--- src/memory.ts
+++ src/memory.ts
@@ -8,17 +8,32 @@ export namespace Memory {
     }
 
     export function readUByte(mem: Memory, addr: number): number {
+        checkImplement(addr);
         return mem[addr];
     }
     export function readSByte(mem: Memory, addr: number): number {
+        checkImplement(addr);
         if (mem[addr] & 0x80) return -(((mem[addr] ^ 0xff) + 1) & 0xff);
         return mem[addr];
     }
     export function readWord(mem: Memory, addr: number): number {
+        checkImplement(addr);
         return (mem[addr + 1] << 8) | mem[addr];
     }
 
     export function writeByte(mem: Memory, addr: number, value: number) {
+        checkImplement(addr);
         return mem[addr] = value;
+    }
+
+    // 未実装なとき例外を出す。デバッグ用
+    function checkImplement(addr: number){
+        // main memory (ROM)
+        if(addr < 0x8000) return;
+
+        // IO
+        if(addr == 0xff44) return;
+
+        throw new Error(`Cannot access: 0x${addr.toString(16)}`);
     }
 }
