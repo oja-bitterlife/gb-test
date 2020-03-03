4a50f38abb9c028820ef55f4f2f72bb8014ebf73
ozaki <bitter.life.club@gmail.com>
2020-03-03 17:36:00 +0000

    OP名追加準備

--- src/op.ts
+++ src/op.ts
@@ -5,7 +5,7 @@ import { Register } from "./register";
 export namespace Op {
     export function process_op_code(op_code: number, gb: GB.env){
         try{
-            op_code_map[op_code](gb);
+            op_code_map[op_code].func(gb);
         }catch(ex){
             // not find instruction
             console.log("addr: 0x" + (gb.regs.pc-1).toString(16) + " => op: 0x" + op_code.toString(16));
@@ -13,30 +13,30 @@ export namespace Op {
         }
     }
 
-    const op_code_map: { [op_code: number]: (gb: GB.env) => void } = {
-        0x00: (_) => {},
-        0x01: (gb) => { gb.regs.c = GB.loadSByte(gb); gb.regs.b = GB.loadSByte(gb); },
-        0x04: (gb) => { gb.regs.b++; },
-        0x05: (gb) => { gb.regs.b--; },
-        0x06: (gb) => { gb.regs.b = GB.loadSByte(gb); },
-        0x0c: (gb) => { gb.regs.c++; },
-        0x0d: (gb) => { gb.regs.c--; },
-        0x0e: (gb) => { gb.regs.c = GB.loadSByte(gb); },
-        0x11: (gb) => { gb.regs.e = GB.loadSByte(gb); gb.regs.d = GB.loadSByte(gb); },  // LD de
-        0x18: (gb) => { const n = GB.loadSByte(gb); gb.regs.pc += n; },
-        0x20: (gb) => { const n = GB.loadSByte(gb); if(!gb.flags.zero) gb.regs.pc += n; },
-        0x21: (gb) => { gb.regs.l = GB.loadSByte(gb); gb.regs.h = GB.loadSByte(gb); },  // LD hl
-        0x31: (gb) => { gb.regs.sp = GB.loadWord(gb); },
-        0x3e: (gb) => { gb.regs.a = GB.loadSByte(gb); },
-        0x76: (gb) => { },  // HALT
-        0xaf: (gb) => { gb.regs.a ^= gb.regs.a; Register.updateFlags(gb, gb.regs.a, false); },
-        0xc3: (gb) => { gb.regs.pc = GB.loadWord(gb); },
-        0xc9: (gb) => { gb.regs.pc = GB.popWord(gb); },
-        0xcd: (gb) => { const n = GB.loadWord(gb); GB.pushWord(gb, gb.regs.pc); gb.regs.pc = n; },
-        0xe0: (gb) => { Memory.writeByte(gb.mem, 0xFF00 | GB.loadUByte(gb), gb.regs.a); },
-        0xf0: (gb) => { gb.regs.a = Memory.readUByte(gb.mem, 0xff00 | GB.loadUByte(gb)); },
-        0xf3: (gb) => { gb.regs.ie = false; },
-        0xfe: (gb) => { Register.updateFlags(gb, GB.loadUByte(gb), true) },
+    const op_code_map: { [op_code: number]: {func: (gb: GB.env) => void} } = {
+        0x00: {func: (gb) => {}},
+        0x01: {func: (gb) => { gb.regs.c = GB.loadSByte(gb); gb.regs.b = GB.loadSByte(gb); }},
+        0x04: {func: (gb) => { gb.regs.b++; }},
+        0x05: {func: (gb) => { gb.regs.b--; }},
+        0x06: {func: (gb) => { gb.regs.b = GB.loadSByte(gb); }},
+        0x0c: {func: (gb) => { gb.regs.c++; }},
+        0x0d: {func: (gb) => { gb.regs.c--; }},
+        0x0e: {func: (gb) => { gb.regs.c = GB.loadSByte(gb); }},
+        0x11: {func: (gb) => { gb.regs.e = GB.loadSByte(gb); gb.regs.d = GB.loadSByte(gb); }},  // LD de
+        0x18: {func: (gb) => { const n = GB.loadSByte(gb); gb.regs.pc += n; }},
+        0x20: {func: (gb) => { const n = GB.loadSByte(gb); if(!gb.flags.zero) gb.regs.pc += n; }},
+        0x21: {func: (gb) => { gb.regs.l = GB.loadSByte(gb); gb.regs.h = GB.loadSByte(gb); }},  // LD hl
+        0x31: {func: (gb) => { gb.regs.sp = GB.loadWord(gb); }},
+        0x3e: {func: (gb) => { gb.regs.a = GB.loadSByte(gb); }},
+        0x76: {func: (gb) => { }},  // HALT
+        0xaf: {func: (gb) => { gb.regs.a ^= gb.regs.a; Register.updateFlags(gb, gb.regs.a, false); }},
+        0xc3: {func: (gb) => { gb.regs.pc = GB.loadWord(gb); }},
+        0xc9: {func: (gb) => { gb.regs.pc = GB.popWord(gb); }},
+        0xcd: {func: (gb) => { const n = GB.loadWord(gb); GB.pushWord(gb, gb.regs.pc); gb.regs.pc = n; }},
+        0xe0: {func: (gb) => { Memory.writeByte(gb.mem, 0xFF00 | GB.loadUByte(gb), gb.regs.a); }},
+        0xf0: {func: (gb) => { gb.regs.a = Memory.readUByte(gb.mem, 0xff00 | GB.loadUByte(gb)); }},
+        0xf3: {func: (gb) => { gb.regs.ie = false; }},
+        0xfe: {func: (gb) => { Register.updateFlags(gb, GB.loadUByte(gb), true) }},
     };
 
     // op cycles table
