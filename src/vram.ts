1d37bc25ecb103fe81f0ff595b71acec2dea74f3
ozaki <bitter.life.club@gmail.com>
2020-03-06 19:59:34 +0000

    pixls.txt排除

--- src/vram.ts
+++ src/vram.ts
@@ -10,8 +10,8 @@ export namespace Vram {
     export function getPixels(mem: Memory.Memory) {
         let pixels = new Uint8Array(HEIGHT * WIDTH);
         for (let i = 0; i < HEIGHT * WIDTH; i++) {
-            pixels[i] = (Memory.readUByte(mem, ADDR + (i/8*2|0) + 1) >> (7-i%8)) & 1
-                      | (Memory.readUByte(mem, ADDR + (i/8*2|0)) >> (7-i%8)) & 1;
+            pixels[i] = (((Memory.readUByte(mem, ADDR + (i/8|0)*2 + 1) >> (7-i%8)) & 1) << 1)
+                      | (Memory.readUByte(mem, ADDR + (i/8|0)*2) >> (7-i%8)) & 1;
         }
         return pixels;
     }
