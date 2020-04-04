16e02c30bc4725580121434c825f3cef94e58b46
ozaki <bitter.life.club@gmail.com>
2020-04-04 19:59:56 +0000

    screen_y実装開始

--- src/vram.ts
+++ src/vram.ts
@@ -1,9 +1,7 @@
 import { Memory } from "./memory";
 
 export namespace Vram {
-    // resolution
-    const WIDTH = 256;
-    const HEIGHT = 144;
+    let bg_buf = new Uint8Array(256*256);
 
     const TILE_0 = 0x8800;  // 8000-8FFF
     const TILE_1 = 0x8000;  // 8800-97FF
@@ -28,15 +26,31 @@ export namespace Vram {
         return tile;
     };
 
-    const drawTileToPixelBuf = (buf_256x256: Uint8Array, buf_x: number, buf_y: number, tile_8x8: Uint8Array): void => {
+    const drawTileToBGBuf = (buf_x: number, buf_y: number, tile_8x8: Uint8Array): void => {
         for (let y = 0; y < 8; y++) {
             for (let x = 0; x < 8; x++) {
-                buf_256x256[(buf_y + y) * 256 + buf_x + x] = tile_8x8[y * 8 + x];
+                bg_buf[(buf_y + y) * 256 + buf_x + x] = tile_8x8[y * 8 + x];
             }
         }
     };
     export const getScreen = (mem: Uint8Array): Uint8Array => {
-        let pixels = new Uint8Array(256 * 256);
+        updateBGBuf(mem);
+
+        const wy = 0;
+        const wx = 0;
+
+        let pixels = new Uint8Array(160 * 144);
+        for (let y = 0; y < 144; y++) {
+            for (let x = 0; x < 160; x++) {
+                pixels[y*160+x] = bg_buf[(wy+y)*256+(wx+x)];
+            }
+        }
+        return pixels;
+    };
+
+    export const updateBGBuf = (mem: Uint8Array) => {
+        const sy = 0x8c;
+        const sx = 0;
 
         const offset = (Memory.readUByte(mem, 0xff40) & (1<<6)) == 0 ? WINDOW_0 : WINDOW_1;
 
@@ -44,10 +58,8 @@ export namespace Vram {
             for (let x = 0; x < 32; x++) {
                 const tile_no = mem[offset + (y * 32 + x)];
                 const tile = getTile(mem, tile_no);
-                drawTileToPixelBuf(pixels, x*8, y*8, tile);
+                drawTileToBGBuf(sx+x*8, sy+y*8, tile);
             }
         }
-
-        return pixels;
-    };
+    }
 }
