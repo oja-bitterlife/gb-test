596effc5acb88b8263115cae82b755ae085014ee
ozaki <bitter.life.club@gmail.com>
2020-04-05 15:06:11 +0000

    bg回り込み追加

--- src/vram.ts
+++ src/vram.ts
@@ -5,6 +5,8 @@ export namespace Vram {
 
     const TILE_0 = 0x8800;  // 8000-8FFF
     const TILE_1 = 0x8000;  // 8800-97FF
+    const BG_0 = 0x9800;  // 9800-9BFF
+    const BG_1 = 0x9c00;  // 9C00-9FFF
     const WINDOW_0 = 0x9800;  // 9800-9BFF
     const WINDOW_1 = 0x9c00;  // 9C00-9FFF
 
@@ -28,8 +30,10 @@ export namespace Vram {
 
     const drawTileToBGBuf = (buf_x: number, buf_y: number, tile_8x8: Uint8Array): void => {
         for (let y = 0; y < 8; y++) {
+            const yy = (buf_y + y) % 256;
             for (let x = 0; x < 8; x++) {
-                bg_buf[(buf_y + y) * 256 + buf_x + x] = tile_8x8[y * 8 + x];
+                const xx = (buf_x + x) % 256;
+                bg_buf[yy * 256 + xx] = tile_8x8[y * 8 + x];
             }
         }
     };
@@ -39,20 +43,27 @@ export namespace Vram {
         const wy = 0;
         const wx = 0;
 
-        let pixels = new Uint8Array(160 * 144);
-        for (let y = 0; y < 144; y++) {
-            for (let x = 0; x < 160; x++) {
-                pixels[y*160+x] = bg_buf[(wy+y)*256+(wx+x)];
+        let pixels = new Uint8Array(256 * 256);
+        for (let y = 0; y < 256; y++) {
+            for (let x = 0; x < 256; x++) {
+                pixels[y*256+x] = bg_buf[(wy+y)*256+(wx+x)];
             }
         }
         return pixels;
     };
 
     export const updateBGBuf = (mem: Uint8Array) => {
-        const sy = 0x8c;
-        const sx = 0;
+        // clear
+        for (let y = 0; y < 256; y++) {
+            for (let x = 0; x < 256; x++) {
+                bg_buf[y*256+x] = 0;
+            }
+        }
 
-        const offset = (Memory.readUByte(mem, 0xff40) & (1<<6)) == 0 ? WINDOW_0 : WINDOW_1;
+        const sy = mem[0xff42];
+        const sx = mem[0xff43];
+
+        const offset = (Memory.readUByte(mem, 0xff40) & (1<<3)) == 0 ? BG_0 : BG_1;
 
         for (let y = 0; y < 32; y++) {
             for (let x = 0; x < 32; x++) {
