f8f3417c4f519822ada345e9a2dc9c552c36541c
ozaki <bitter.life.club@gmail.com>
2020-04-09 16:37:03 +0000

    bg表示修正

--- src/vram.ts
+++ src/vram.ts
@@ -1,8 +1,8 @@
 import { Memory } from "./memory";
 
 export namespace Vram {
-    const TILE_0 = 0x9000;  // 8000-8FFF
-    const TILE_1 = 0x8000;  // 8800-97FF
+    const TILE_0 = 0x9000;  // 8800-97FF
+    const TILE_1 = 0x8000;  // 8000-8FFF
     const BG_0 = 0x9800;  // 9800-9BFF
     const BG_1 = 0x9c00;  // 9C00-9FFF
     const WINDOW_0 = 0x9800;  // 9800-9BFF
@@ -12,37 +12,50 @@ export namespace Vram {
     export const getScreen = (mem: Uint8Array): Uint8Array => {
         let screen_buf = new Uint8Array(160 * 144);
 
-        const bg_buf = createBGBuf(mem);
+        // BG Enable
+        if(Memory.readUByte(mem, 0xff40) & (1 << 0)){
+            const bg_buf = createBGBuf(mem);
 
-        const sy = mem[0xff42];
-        const sx = mem[0xff43];
+            const sy = Memory.readUByte(mem, 0xff42);
+            const sx = Memory.readUByte(mem, 0xff43);
 
-        for (let y = 0; y < 144; y++) {
-            for (let x = 0; x < 160; x++) {
-                screen_buf[y * 160 + x] = bg_buf[(sy + y) % 256 * 256 + (sx + x) % 256];
+            for (let y = 0; y < 144; y++) {
+                for (let x = 0; x < 160; x++) {
+                    screen_buf[y * 160 + x] = bg_buf[(sy + y) % 256 * 256 + (sx + x) % 256];
+                }
             }
         }
 
-        const [win_buf, wx, wy] = createWinBuf(mem);
+        // Win Enable
+        if(Memory.readUByte(mem, 0xff40) & (1 << 5)){
+            const win_buf = createWinBuf(mem);
 
-        // merge
-        for (let y = 0; y < 144; y++) {
-            for (let x = 0; x < 160; x++) {
-//                screen_buf[y * 160 + x] = win_buf[(wy + y) % 256 * 256 + (wx + x) % 256];
+            // merge
+            const wy = Memory.readUByte(mem, 0xff4a);
+            const wx = Memory.readUByte(mem, 0xff4b) - 7;
+            for (let y = wy; y < 144; y++) {
+                for (let x = wx; x < 160; x++) {
+                    const palette = win_buf[(wy + y) % 256 * 256 + (wx + x) % 256];
+                    if(palette) screen_buf[y * 160 + x] = palette;
+                }
             }
         }
 
         return screen_buf;
     };
-    export const createWinBuf = (mem: Uint8Array): [Uint8Array, number, number] => {
+    export const createWinBuf = (mem: Uint8Array): Uint8Array => {
         let win_buf = new Uint8Array(255 * 255);
-
-        const wy = Memory.readUByte(mem, 0xff4a);
-        const wx = Memory.readUByte(mem, 0xff4b);
-
         const win_addr = (Memory.readUByte(mem, 0xff40) & (1 << 6)) == 0 ? WINDOW_0 : WINDOW_1;
 
-        return [win_buf, wx, wy];
+        // to pixels
+        for (let y = 0; y < 32; y++) {
+            for (let x = 0; x < 32; x++) {
+                const tile = getTile(mem, mem[win_addr + (y * 32 + x)]);
+                drawTileToBuf(win_buf, tile, x * 8, y * 8);
+            }
+        }
+
+        return win_buf;
     }
 
     export const createBGBuf = (mem: Uint8Array): Uint8Array => {
@@ -65,7 +78,7 @@ export namespace Vram {
 
         const tile_mode = (Memory.readUByte(mem, 0xff40) >> 4) & 1;
 
-        if(tile_mode && no >= 128) no = 256-no;  // signed
+        if(tile_mode == 0 && no >= 128) no = no-256;  // signed
         const offset = [TILE_0, TILE_1][tile_mode] + no * 8 * 2;
 
         for (let y = 0; y < 8; y++) {
